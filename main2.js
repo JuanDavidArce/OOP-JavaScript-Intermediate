eaa7d2dcaabb061a26a47560c103fd8253f36e0d
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-01 14:00:54 -0500

    Recursivity

--- main2.js
+++ main2.js
@@ -1,11 +1,33 @@
-const obj1 = {
-    a:'a',
-    b:'b',
-    c: {
-        d:'d',
-        e:'e',
+// const obj1 = {
+//     a:'a',
+//     b:'b',
+//     c: {
+//         d:'d',
+//         e:'e',
+//     }
+// }
+
+// const sringifiedComplexObje= JSON.stringify(obj1) //Funciona simpre y cuando no usemos metodos
+// const obj2 = JSON.parse(sringifiedComplexObje)
+
+function recursiva(numbersArray)
+{
+    if(numbersArray.length !=0)
+    {
+        const firstNum=numbersArray[0];
+        console.log(firstNum)
+        numbersArray.shift(); //Elimina el primer elemento del array
+        return recursiva(numbersArray);
     }
+    
 }
 
-const sringifiedComplexObje= JSON.stringify(obj1) //Funciona simpre y cuando no usemos metodos
-const obj2 = JSON.parse(sringifiedComplexObje)
+const numeritos=[0,1,2,3,4,5,6,7,8,9]
+recursiva(numeritos)
+// let num=0
+
+// for(let index=0;index<numeritos.length;index++)
+// {   
+//     var numerito = numeritos[index];
+//     console.log({index,numerito});
+// }
